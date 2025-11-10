923fd42c3402db5bf23388cc0af01043dd64285f
vufimcev <buktor@bk.ru>
2025-11-09 18:47:54 -0800

    feat(binary-search-tree): execute add method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,19 +1,39 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-  root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.rootNode = null;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  root() {
+    return this.rootNode;
+  }
+
+  add( data ) {
+    this.rootNode = addWithin(this.rootNode, data);
+
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value);
+      }
+
+      if (node.data === value) {
+        return node;
+      }
+
+      if (value < node.data) {
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+
+      return node;
+    }
   }
 
   find(/* data */) {
